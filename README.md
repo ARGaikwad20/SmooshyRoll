028ea4ba1b30deae12f6c87d47276e695a091f21
ARGaikwad20 <ankitgaikwad8@gmail.com>
2026-02-08 14:02:32 +0530

    Initial commit: React Vite TypeScript with Tailwind

---
+++ README.md
@@ -0,0 +1,1 @@
+MUSHYROLL App
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="read-the-docs">Welcome to Mushyroll.</p>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
